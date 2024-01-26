1dfe8d404ccc45b3b054c6f34ce42b241fdcd566
abyanmajid <abyan@abydyl.net>
2024-01-26 20:26:26 +0700

    refactor: set cors to allow vercel

--- server/src/main.rs
+++ server/src/main.rs
@@ -30,6 +30,11 @@ async fn serve(listen: bool, require_logging: bool, args: CommandLines) -> Resul
 
     let cors = CorsLayer::new()
         .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_origin(
+            "https://revise-badvcomp.vercel.app"
+                .parse::<HeaderValue>()
+                .unwrap(),
+        )
         .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
         .allow_credentials(true)
         .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
